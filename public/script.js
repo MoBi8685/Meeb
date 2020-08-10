47559ed7fb94c1b8698f497a28679e40a878b1c5
Mo The B <momo.2002.812@gmail.com>
2020-08-10 14:38:03 +0300

    Enabled Voice Chat

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ const peers = {}
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
